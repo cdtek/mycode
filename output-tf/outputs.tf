ba3cef79a8dc7e44ced233fa1fbd5e35d2892428
cdtek <christiandelgado@outlook.com>
2022-10-24 19:08:30 +0000

    docker output values

---
+++ output-tf/outputs.tf
@@ -0,0 +1,16 @@
+# produces an output value named "container_id"
+output "container_id" {
+  description = "ID of the Docker container"
+  value       = docker_container.nginx.id
+}
+# produces an output value named "image_id"
+output "image_id" {
+  description = "ID of the Docker image"
+  value       = docker_image.nginx.id
+}
+
+# produces an output value named "container_hostname"
+output "container_hostname" {
+  description = "hostname of container"
+  value       = docker_container.nginx.hostname
+}
